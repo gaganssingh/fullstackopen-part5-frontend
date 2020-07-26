4a7cae92eb27e16b8a0a736b2bbc9b3251cd5cf9
Gagan Singh <gagansingh.dev@outlook.com>
2020-07-26 18:32:05 -0400

    Refactored DisplayBlog into its own component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { useState, useEffect } from "react";
-import Blog from "./components/Blog";
+
 import blogService from "./services/blogs";
 import loginService from "./services/login";
 import Notification from "./components/Notification";
-import NewBlog from "./components/NewBlog";
+
 import LoginForm from "./components/LoginForm";
+import DisplayBlog from "./components/DisplayBlog";
 
 const App = () => {
    const [blogs, setBlogs] = useState([]);
-
    const [user, setUser] = useState(null);
-
    const [notification, setNotification] = useState("");
    const [notificationStyle, setNotificationStyle] = useState("");
-   const [showBlogForm, setShowBlogForm] = useState(false);
 
    //  Initial loading of all blogs when app first loads
    useEffect(() => {
@@ -90,23 +88,6 @@ const App = () => {
       }, 3000);
    };
 
-   const displayBlog = () => (
-      <div>
-         <h2>blogs</h2>
-         <button onClick={() => setShowBlogForm(true)}>Create Blog</button>
-         {showBlogForm && (
-            <NewBlog
-               createBlog={createBlog}
-               setShowBlogForm={setShowBlogForm}
-            />
-         )}
-         <br />
-         {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} />
-         ))}
-      </div>
-   );
-
    return (
       <div>
          <h2>{!user ? "Please login" : "Blogs"}</h2>
@@ -115,11 +96,13 @@ const App = () => {
          )}
          {user && <p>{user.name} is currently logged in</p>}
          {user && <button onClick={handleLogout}>logout</button>}
-         {!user ? <LoginForm handleLogin={handleLogin} /> : displayBlog()}
+         {!user ? (
+            <LoginForm handleLogin={handleLogin} />
+         ) : (
+            <DisplayBlog createBlog={createBlog} blogs={blogs} />
+         )}
       </div>
    );
-
-   //  return ;
 };
 
 export default App;
---
+++ src/components/DisplayBlog.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+import NewBlog from "./NewBlog";
+import Blog from "./Blog";
+
+const DisplayBlog = ({ createBlog, blogs }) => {
+   const [showBlogForm, setShowBlogForm] = useState(false);
+
+   return (
+      <div>
+         <h2>blogs</h2>
+         <button onClick={() => setShowBlogForm(true)}>Create Blog</button>
+         {showBlogForm && (
+            <NewBlog
+               createBlog={createBlog}
+               setShowBlogForm={setShowBlogForm}
+            />
+         )}
+         <br />
+         {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+         ))}
+      </div>
+   );
+};
+
+export default DisplayBlog;
