8121a0a7062a108f2a06634b4a5e8dacc6a228c9
Gagan Singh <gagansingh.dev@outlook.com>
2020-07-26 14:48:19 -0400

    Finished Ex. 5.1 to 5.4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,183 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+import Notification from "./components/Notification";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+   const [blogs, setBlogs] = useState([]);
+   const [username, setUsername] = useState("");
+   const [password, setPassword] = useState("");
+   const [user, setUser] = useState(null);
+   const [newTitle, setNewTitle] = useState("");
+   const [newAuthor, setNewAuthor] = useState("");
+   const [newUrl, setNewUrl] = useState("");
+   const [notification, setNotification] = useState("");
+   const [notificationStyle, setNotificationStyle] = useState("");
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+   //  Initial loading of all blogs when app first loads
+   useEffect(() => {
+      blogService.getAll().then((blogs) => setBlogs(blogs));
+   }, []);
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+   //  Check if user already logged in on first app load
+   useEffect(() => {
+      const loggedUserJSON = window.localStorage.getItem("loggedInBlogAppUser");
+      if (loggedUserJSON) {
+         const user = JSON.parse(loggedUserJSON);
+         setUser(user);
+      }
+   }, []);
 
-export default App
+   const handleLogin = async (event) => {
+      event.preventDefault();
+
+      try {
+         const user = await loginService.login({
+            username,
+            password,
+         });
+
+         window.localStorage.setItem(
+            "loggedInBlogAppUser",
+            JSON.stringify(user)
+         );
+         blogService.setToken(user.token);
+         setUser(user);
+         setUsername("");
+         setPassword("");
+         setNotification("Successfully logged in");
+         setNotificationStyle("success");
+      } catch (exception) {
+         setNotification("Invalid login credentials");
+         setNotificationStyle("error");
+      }
+
+      setTimeout(() => {
+         setNotification("");
+         setNotificationStyle("");
+      }, 3000);
+   };
+
+   const handleLogout = () => {
+      window.localStorage.removeItem("loggedInBlogAppUser");
+      setUser(null);
+      setNotification("Successfully logged out");
+      setNotificationStyle("success");
+
+      setTimeout(() => {
+         setNotification("");
+         setNotificationStyle("");
+      }, 3000);
+   };
+
+   const addBlog = async (e) => {
+      console.log("clicked");
+      e.preventDefault();
+
+      const newBlog = {
+         title: newTitle,
+         author: newAuthor,
+         url: newUrl,
+         likes: Math.floor(Math.random() * 100),
+      };
+      console.log(newBlog);
+
+      blogService.setToken(user.token);
+
+      try {
+         const blogObject = await blogService.create(newBlog);
+         setBlogs(blogs.concat(blogObject));
+         setNewTitle("");
+         setNewAuthor("");
+         setNewUrl("");
+         setNotification(
+            `A new blog titled "${blogObject.title}" by ${blogObject.author} added.`
+         );
+         setNotificationStyle("success");
+      } catch (error) {
+         console.log(error);
+         //  setNotification();
+         setNotificationStyle("error");
+      }
+
+      setTimeout(() => {
+         setNotification("");
+         setNotificationStyle("");
+      }, 3000);
+   };
+
+   const loginForm = () => (
+      <form onSubmit={handleLogin}>
+         <label htmlFor="username">username</label>
+         <input
+            type="text"
+            name="username"
+            id="username"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+         />
+         <label htmlFor="password">password</label>
+         <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+         />
+         <button>login</button>
+      </form>
+   );
+
+   const displayBlog = () => (
+      <div>
+         <h2>blogs</h2>
+         <form onSubmit={addBlog}>
+            <h2>Create new blog</h2>
+            title:
+            <input
+               type="text"
+               value={newTitle}
+               onChange={({ target }) => setNewTitle(target.value)}
+            />
+            <div>
+               author:
+               <input
+                  type="text"
+                  value={newAuthor}
+                  onChange={({ target }) => setNewAuthor(target.value)}
+               />
+            </div>
+            <div>
+               url:
+               <input
+                  type="text"
+                  value={newUrl}
+                  onChange={({ target }) => setNewUrl(target.value)}
+               />
+            </div>
+            <button>create</button>
+         </form>
+         <br />
+         {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+         ))}
+      </div>
+   );
+
+   return (
+      <div>
+         <h2>{!user ? "Please login" : "Blogs"}</h2>
+         {notification && (
+            <Notification text={notification} style={notificationStyle} />
+         )}
+         {user && <p>{user.name} is currently logged in</p>}
+         {user && <button onClick={handleLogout}>logout</button>}
+         {!user ? loginForm() : displayBlog()}
+      </div>
+   );
+
+   //  return ;
+};
+
+export default App;
